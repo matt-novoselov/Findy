62388c911c6f5cdd9a41c1d1c3b84576a24fc76b
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-01 16:31:00 +0100

    Moved speech synthesizer

---
+++ Findy.swiftpm/My App/SpeechSynthesizer/SpeechSynthesizer.swift
@@ -0,0 +1,47 @@
+//
+//  SpeechSynthesizer.swift
+//  Findy
+//
+//  Created by Matt Novoselov on 01/02/25.
+//
+
+import AVFoundation
+
+@MainActor
+@Observable
+class SpeechSynthesizer: NSObject, AVSpeechSynthesizerDelegate {
+    private let synthesizer = AVSpeechSynthesizer()
+    private var utteranceQueue: [AVSpeechUtterance] = []
+    
+    override init() {
+        super.init()
+        synthesizer.delegate = self
+    }
+    
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(identifier: AppMetrics.speechVoiceIdentifier)
+        utterance.rate = AVSpeechUtteranceDefaultSpeechRate
+        enqueue(utterance)
+    }
+    
+    private func enqueue(_ utterance: AVSpeechUtterance) {
+        utteranceQueue.append(utterance)
+        if !synthesizer.isSpeaking {
+            speakNext()
+        }
+    }
+    
+    private func speakNext() {
+        guard !utteranceQueue.isEmpty else { return }
+        let nextUtterance = utteranceQueue.removeFirst()
+        synthesizer.speak(nextUtterance)
+    }
+    
+    // MARK: - AVSpeechSynthesizerDelegate
+    nonisolated func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        Task { @MainActor in
+            speakNext()
+        }
+    }
+}
