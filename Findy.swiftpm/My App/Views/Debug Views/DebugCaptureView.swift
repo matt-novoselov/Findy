8c03a8e85fa348bd69e3e0b7ad8c0879f9870005
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-06 14:12:20 +0100

    Added debug visualization to visualize cut out object

--- Findy.swiftpm/My App/Views/Debug Views/DebugCaptureView.swift
+++ Findy.swiftpm/My App/Views/Debug Views/DebugCaptureView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 struct DebugCaptureView: View {
     
     @Environment(ARCoordinator.self) private var arCoordinator
+    @Environment(AppViewModel.self) private var appViewModel
     
     var body: some View {
+        let mostProminentResult = selectMostProminentObservation(
+            from: arCoordinator.detectionResults,
+            targetObject: appViewModel.targetDetectionObject
+        )
+        
         ZStack {
             if let captureImage = arCoordinator.normalizedCaptureImage, let cgImage = captureImage.toCGImage() {
                 Image(decorative: cgImage, scale: 7)
                     .clipShape(.rect(cornerRadius: 4))
+                
+                if let mostProminentResult {
+                    CroppedImage(cgImage: cgImage, cropRect: mostProminentResult.boundingBox)
+                        .frame(height: 100)
+                } else {
+                    Text("No object detected")
+                }
             } else {
                 Text("No capture")
             }
@@ -25,5 +38,21 @@ struct DebugCaptureView: View {
         .clipShape(.rect(cornerRadius: 8))
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
         .padding()
+    }
+}
+
+struct CroppedImage: View {
+    let cgImage: CGImage
+    let cropRect: CGRect
+    
+    var body: some View {
+        Image(uiImage: UIImage(cgImage: cropImage(cgImage, to: cropRect)!))
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func cropImage(_ image: CGImage, to rect: CGRect) -> CGImage? {
+        guard rect.size.width > 0, rect.size.height > 0 else { return nil }
+        return image.cropping(to: rect)
     }
 }
