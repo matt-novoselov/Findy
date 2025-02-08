aae61e45cd26989d7545de4fcece3562eedb444c
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-08 23:04:40 +0100

    Added blur camera effect

--- Findy.swiftpm/My App/Views/ObjectFinderView.swift
+++ Findy.swiftpm/My App/Views/ObjectFinderView.swift
@@ -21,6 +21,21 @@ struct ObjectFinderView: View {
         let arContainer: ARContainer = .init(coordinator: arCoordinator)
         
         arContainer
+        
+//            .brightness(-0.1)
+//            .overlay(
+//                GeometryReader { geometry in
+//                    RadialGradient(
+//                        gradient: Gradient(colors: [.clear, .black]),
+//                        center: .center,
+//                        startRadius: 0,
+//                        endRadius: max(geometry.size.width, geometry.size.height) / 2
+//                    )
+//                    .opacity(1)
+//                }
+//            )
+//            .blur(radius: 20)
+        
             .overlay {
                 DebugObjectDetectionView()
             }
