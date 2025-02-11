6a14a5e8151c6d9a20f41ee5f67eb329819721f6
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-12 00:51:23 +0100

    Refactoring analyze image with ML

--- Findy.swiftpm/My App/Models/AR Container/ARContainer.swift
+++ Findy.swiftpm/My App/Models/AR Container/ARContainer.swift
@@ -1,5 +1,4 @@
 import ARKit
-import CreateML
 import RealityKit
 import SwiftUI
 import Combine
@@ -21,49 +20,6 @@ class ARSceneCoordinator {
     // Detection properties
     private(set) var detectedObjects: [ProcessedObservation] = []
     public var processedFrameImage: CIImage?
-    
-    private func analyzeImageWithML() {
-        // Ensure we have the necessary appViewModel.
-        guard let appViewModel = self.appViewModel else {
-            return
-        }
-        
-        // Select the dominant observation using the target detection object.
-        guard let mostProminentResult = selectDominantObservation(
-            from: self.detectedObjects,
-            targetObject: appViewModel.savedObject.targetDetectionObject
-        ) else {
-            return
-        }
-        
-        // Convert the processed frame image to CGImage.
-        guard let cgImage = self.processedFrameImage?.toCGImage() else {
-            return
-        }
-        
-        // Crop the CGImage based on the detected bounding box.
-        let cropRect = mostProminentResult.boundingBox
-        guard let cgImageCropped = cgImage.cropping(to: cropRect) else {
-            // Log error if needed: cropping failed.
-            return
-        }
-        
-        // Ensure the image classifier is available.
-        guard let imageClassifier = appViewModel.savedObject.imageClassifier else {
-            return
-        }
-        
-        // Attempt prediction and handle possible errors.
-        do {
-            let prediction = try imageClassifier.prediction(from: cgImageCropped)
-            if prediction == "myObject" {
-                self.handleNewDetectionResults()
-            }
-        } catch {
-            // Optionally log the error.
-            return
-        }
-    }
     
     weak var appViewModel: AppViewModel?
     weak var speechSynthesizer: SpeechSynthesizer?
