947fe6d21bdba8f8d6cfb23078cecb08d4b3ca66
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-11 20:59:11 +0100

    Simplified debug view

--- Findy.swiftpm/My App/Views/Debug Views/Components/DebugCaptureView.swift
+++ Findy.swiftpm/My App/Views/Debug Views/Components/DebugCaptureView.swift
@@ -3,48 +3,14 @@ import SwiftUI
 struct DebugCaptureView: View {
     
     @Environment(ARSceneCoordinator.self) private var arCoordinator
-    @Environment(AppViewModel.self) private var appViewModel
     
     var body: some View {
-        let mostProminentResult = selectDominantObservation(
-            from: arCoordinator.detectedObjects,
-            targetObject: appViewModel.savedObject.targetDetectionObject
-        )
-        
-        HStack {
-            if let captureImage = arCoordinator.processedFrameImage,
-               let cgImage = captureImage.toCGImage()
-            {
-                Image(decorative: cgImage, scale: 7)
-                    .clipShape(.rect(cornerRadius: 4))
-                
-                if let mostProminentResult = mostProminentResult {
-                    CroppedImage(cgImage: cgImage, cropRect: mostProminentResult.boundingBox)
-                        .frame(height: 100)
-                } else {
-                    Text("No object detected")
-                }
-            } else {
-                Text("No capture")
-            }
+        if let captureImage = arCoordinator.processedFrameImage,
+           let cgImage = captureImage.toCGImage(){
+            Image(decorative: cgImage, scale: 7)
+                .padding()
+        } else {
+            Text("No capture")
         }
-        .padding()
-    }
-}
-
-struct CroppedImage: View {
-    let cgImage: CGImage
-    let cropRect: CGRect
-    @Environment(AppViewModel.self) private var appViewModel
-    
-    var body: some View {
-        Image(uiImage: UIImage(cgImage: cropImage(cgImage, to: cropRect)!))
-            .resizable()
-            .scaledToFit()
-    }
-    
-    func cropImage(_ image: CGImage, to rect: CGRect) -> CGImage? {
-        guard rect.size.width > 0, rect.size.height > 0 else { return nil }
-        return image.cropping(to: rect)
     }
 }
