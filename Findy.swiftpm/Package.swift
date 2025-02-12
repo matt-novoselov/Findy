1a63d17aecd03f9f192c4468f199b80019c0e152
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-12 01:11:26 +0100

    New AppIcon

--- Findy.swiftpm/Package.swift
+++ Findy.swiftpm/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             teamIdentifier: "GXDG2KXXXC",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .location),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [
                 .pad,
