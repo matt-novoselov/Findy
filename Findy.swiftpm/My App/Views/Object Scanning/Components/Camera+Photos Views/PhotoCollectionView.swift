aa76e70b2059ff3582f749ae7ec0dc7d6577267f
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-14 17:37:27 +0100

    Photo capture animation

--- Findy.swiftpm/My App/Views/Object Scanning/Components/Camera+Photos Views/PhotoCollectionView.swift
+++ Findy.swiftpm/My App/Views/Object Scanning/Components/Camera+Photos Views/PhotoCollectionView.swift
@@ -4,23 +4,46 @@ struct PhotoCollectionView: View {
     @Environment(AppViewModel.self) private var appViewModel
     
     var body: some View {
-        ForEach(Array(appViewModel.savedObject.takenPhotos.enumerated()), id: \.offset) { index, capturedPhoto in
-            ImageCollectionView(photo: capturedPhoto.photo, index: index)
+        GeometryReader{ proxy in
+            ForEach(Array(appViewModel.savedObject.takenPhotos.enumerated()), id: \.offset) { index, capturedPhoto in
+                ImageCollectionView(photo: capturedPhoto.photo, index: index, geometryReaderSize: proxy.size)
+            }
         }
+        .ignoresSafeArea()
     }
 }
 
 struct ImageCollectionView: View {
     var photo: CGImage
     var index: Int
+    var geometryReaderSize: CGSize
+    let imageSize = 100.0
+    @State private var imageScale: CGFloat = 0.2
+    @State private var imageOffset: CGSize
+    
+    init(photo: CGImage, index: Int, geometryReaderSize: CGSize) {
+        self.photo = photo
+        self.index = index
+        self.geometryReaderSize = geometryReaderSize
+        self.imageOffset = .init(width: geometryReaderSize.width - imageSize, height: geometryReaderSize.height/2 - imageSize/2)
+    }
+    
     var body: some View {
         Image(decorative: photo, scale: 20)
             .resizable()
             .scaledToFill()
-            .frame(width: 100, height: 100)
+            .frame(width: imageSize, height: imageSize)
             .clipShape(.rect(cornerRadius: 10))
             .rotationEffect(.degrees(index % 2 == 0 ? 5 : -5), anchor: .bottom)
-//            .offset(x: CGFloat(index) * 8, y: CGFloat(index) * 8)
+            .scaleEffect(imageScale)
+            .offset(imageOffset)
             .shadow(color: .black.opacity(0.2), radius: 4, x: 0, y: 2)
+            .opacity(imageScale)
+            .onAppear{
+                withAnimation{
+                    imageOffset = .init(width: 0, height: geometryReaderSize.height - imageSize)
+                    imageScale = 1
+                }
+            }
     }
 }
