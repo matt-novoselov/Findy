23c5a855bfc01b30d13752982ed56ec03bfbe785
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-16 13:57:15 +0100

    Fixed photo collection view geometry reader

--- Findy.swiftpm/My App/Views/Object Scanning/Components/Camera+Photos Views/PhotoCollectionView.swift
+++ Findy.swiftpm/My App/Views/Object Scanning/Components/Camera+Photos Views/PhotoCollectionView.swift
@@ -4,9 +4,9 @@ struct PhotoCollectionView: View {
     @Environment(AppViewModel.self) private var appViewModel
     
     var body: some View {
-        GeometryReader{ proxy in
+        ZStack {
             ForEach(Array(appViewModel.savedObject.takenPhotos.enumerated()), id: \.offset) { index, capturedPhoto in
-                ImageCollectionView(photo: capturedPhoto.photo, index: index, geometryReaderSize: proxy.size)
+                ImageCollectionView(photo: capturedPhoto.photo, index: index)
             }
         }
         .ignoresSafeArea()
@@ -16,36 +16,32 @@ struct PhotoCollectionView: View {
 struct ImageCollectionView: View {
     var photo: CGImage
     var index: Int
-    var geometryReaderSize: CGSize
-    let imageSize = 100.0
+    let imageSize: CGFloat = 100
     @State private var imageScale: CGFloat = 0.2
-    @State private var imageOffset: CGSize
     @State private var currentRotation: Double = 0
-    
-    init(photo: CGImage, index: Int, geometryReaderSize: CGSize) {
-        self.photo = photo
-        self.index = index
-        self.geometryReaderSize = geometryReaderSize
-        self.imageOffset = .init(width: geometryReaderSize.width - imageSize, height: geometryReaderSize.height/2 - imageSize/2)
-    }
-    
+    @State private var alignment: Alignment = .trailing
+
     var body: some View {
-        Image(decorative: photo, scale: 20)
-            .resizable()
-            .scaledToFill()
-            .frame(width: imageSize, height: imageSize)
-            .clipShape(.rect(cornerRadius: 10))
-            .rotationEffect(.degrees(currentRotation), anchor: .bottom)
-            .scaleEffect(imageScale)
-            .offset(imageOffset)
-            .shadow(color: .black.opacity(0.2), radius: 4, x: 0, y: 2)
-            .opacity(imageScale)
-            .onAppear{
-                withAnimation(.bouncy){
-                    imageOffset = .init(width: 0, height: geometryReaderSize.height - imageSize)
-                    imageScale = 1
-                    currentRotation = index % 2 == 0 ? 5 : -5
-                }
+        ZStack(alignment: alignment) {
+            Color.clear  // Fills the space
+            Image(decorative: photo, scale: 20)
+                .resizable()
+                .scaledToFill()
+                .frame(width: imageSize, height: imageSize)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .rotationEffect(.degrees(currentRotation), anchor: .bottom)
+                .scaleEffect(imageScale)
+                .shadow(color: .black.opacity(0.2), radius: 4, x: 0, y: 2)
+                .opacity(Double(imageScale))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .ignoresSafeArea()
+        .onAppear {
+            withAnimation(.bouncy) {
+                alignment = .bottomLeading
+                imageScale = 1
+                currentRotation = index % 2 == 0 ? 5 : -5
             }
+        }
     }
 }
