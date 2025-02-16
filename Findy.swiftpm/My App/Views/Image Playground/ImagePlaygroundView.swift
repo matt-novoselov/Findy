d19b93215dbf929ef192726390adcffd6db08cb8
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-16 12:26:52 +0100

    Apple Intelligence button

--- Findy.swiftpm/My App/Views/Image Playground/ImagePlaygroundView.swift
+++ Findy.swiftpm/My App/Views/Image Playground/ImagePlaygroundView.swift
@@ -23,22 +23,17 @@ struct ImagePlaygroundView: View {
                 }
             }
             
+            
             if supportsImagePlayground {
-                Button("Show Generation Sheet", systemImage: "apple.intelligence") {
-                    self.concepts = []
-                    let visionClassifications = appViewModel.savedObject.userPickedClassifications
-                    for classification in visionClassifications {
-                        self.concepts.append(.text(classification.description))
-                    }
-                    
-                    if let cutOutImage = appViewModel.savedObject.objectCutOutImage {
-                        let imageWithWhiteBG = cutOutImage.imageWithWhiteBackgroundSquare()
-                        self.sourceImage = Image(uiImage: imageWithWhiteBG)
-                    }
-                    
-                    showImagePlayground = true
+                Button(action: {openImagePlaygrounds()}) {
+                    ImagePlaygroundButtonLabel()
                 }
+                .tint(.white)
+                .fontDesign(.rounded)
+                .fontWeight(.semibold)
+                .foregroundStyle(.black)
                 .buttonStyle(.borderedProminent)
+                .clipShape(.capsule)
                 .imagePlaygroundSheet(isPresented: $showImagePlayground, concepts: concepts, sourceImage: sourceImage) { url in
                     appViewModel.savedObject.appleIntelligencePreviewImage = url
                 }
@@ -52,4 +47,80 @@ struct ImagePlaygroundView: View {
             }
         }
     }
+    
+    func openImagePlaygrounds(){
+        self.concepts = []
+        let visionClassifications = appViewModel.savedObject.userPickedClassifications
+        for classification in visionClassifications {
+            self.concepts.append(.text(classification.description))
+        }
+        
+        if let cutOutImage = appViewModel.savedObject.objectCutOutImage {
+            let imageWithWhiteBG = cutOutImage.imageWithWhiteBackgroundSquare()
+            self.sourceImage = Image(uiImage: imageWithWhiteBG)
+        }
+        
+        showImagePlayground = true
+    }
+}
+
+struct ImagePlaygroundButtonLabel: View {
+    var body: some View {
+        HStack{
+            Group{
+                Image(systemName: "apple.intelligence")
+                    .foregroundStyle(.white)
+                    .overlay(
+                        ImagePlaygroundLabelGradient()
+                            .blur(radius: 5)
+                            .rotationEffect(.init(degrees: 180))
+                            .mask(Image(systemName: "apple.intelligence"))
+                    )
+                
+                Text("Generate Preview Image")
+            }
+            .font(.body)
+        }
+    }
+}
+
+struct ImagePlaygroundLabelGradient: View {
+    var body: some View {
+        VStack(spacing: -10){
+            HStack(spacing: -10){
+                Group{
+                    Circle()
+                        .foregroundStyle(Color(hex: 0x03AAF6))
+                    Circle()
+                        .foregroundStyle(Color(hex: 0xED75FA))
+                }
+                .frame(width: 20, height: 20)
+            }
+            
+            HStack(spacing: -10){
+                Group{
+                    Circle()
+                        .foregroundStyle(Color(hex: 0xF62B6A))
+                    Circle()
+                        .foregroundStyle(Color(hex: 0xF79B1C))
+                    
+                    Circle()
+                        .foregroundStyle(Color(hex: 0xDB7FFB))
+                }
+                .frame(width: 20, height: 20)
+            }
+        }
+    }
+}
+
+#Preview {
+    Button(action: {}) {
+        ImagePlaygroundButtonLabel()
+    }
+    .tint(.white)
+    .fontDesign(.rounded)
+    .fontWeight(.semibold)
+    .foregroundStyle(.black)
+    .buttonStyle(.borderedProminent)
+    .clipShape(.capsule)
 }
