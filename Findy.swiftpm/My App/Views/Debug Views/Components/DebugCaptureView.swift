c0733d7839a3b573bbe76fa153849b9de4902954
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-16 13:27:25 +0100

    Fixed debug capture

--- Findy.swiftpm/My App/Views/Debug Views/Components/DebugCaptureView.swift
+++ Findy.swiftpm/My App/Views/Debug Views/Components/DebugCaptureView.swift
@@ -4,11 +4,16 @@ struct DebugCaptureView: View {
     
     @Environment(ARSceneCoordinator.self) private var arCoordinator
     @Environment(AppViewModel.self) private var appViewModel
+    let cornerRadius: CGFloat = 10
     
     var body: some View {
         if let captureImage = arCoordinator.processedFrameImage, let cgImage = captureImage.toCGImage() {
             Image(decorative: cgImage, scale: 7)
-                .clipShape(.rect(cornerRadius: 4))
+                .clipShape(.rect(cornerRadius: cornerRadius))
+                .padding(.all, 4)
+                .background(Material.ultraThin, in: .rect(cornerRadius: cornerRadius + 4))
+                .padding()
+                .padding(.top)
         } else {
             Text("No capture")
                 .fontDesign(.rounded)
