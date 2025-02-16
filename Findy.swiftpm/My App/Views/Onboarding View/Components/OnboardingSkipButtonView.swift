3b31843f68c3d8a5e486a233ea26e93d13754e67
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-16 22:20:18 +0100

    Skip onboarding alert

--- Findy.swiftpm/My App/Views/Onboarding View/Components/OnboardingSkipButtonView.swift
+++ Findy.swiftpm/My App/Views/Onboarding View/Components/OnboardingSkipButtonView.swift
@@ -1,22 +1,32 @@
 import SwiftUI
 
-
 struct OnboardingSkipButtonView: View {
     let action: () -> Void
+    @State private var showingConfirmation: Bool = false
+
     var body: some View {
-        Button(action: action) {
+        Button(action: {
+            showingConfirmation = true
+        }) {
             Text("Skip Onboarding")
                 .fontDesign(.rounded)
                 .padding(6)
                 .padding(.horizontal, 8)
                 .font(.callout)
                 .fontWeight(.semibold)
-                .fontDesign(.rounded)
                 .foregroundStyle(.secondary)
                 .glassBackground(cornerRadius: .infinity)
                 .padding(20)
                 .padding(.top)
         }
         .buttonStyle(.plain)
+        .alert("Skip Onboarding", isPresented: $showingConfirmation) {
+            Button("Cancel", role: .cancel) { }
+            Button("Skip Onboarding", role: .destructive) {
+                action()
+            }
+        } message: {
+            Text("Are you sure you want to skip onboarding?")
+        }
     }
 }
