05f3102995bd9c7f00f0b423890954b7c4e18103
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-18 10:55:29 +0100

    Make instructions shorter

--- Findy.swiftpm/My App/Views/Onboarding View/Components/Cards View Model/ObjectScanViewModel.swift
+++ Findy.swiftpm/My App/Views/Onboarding View/Components/Cards View Model/ObjectScanViewModel.swift
@@ -2,21 +2,22 @@ import Foundation
 
 class ObjectScanViewModel {
     let cards: [OnboardingCardDescriptionModel] = [
-        OnboardingCardDescriptionModel(
-            icon: "cube.box.fill",
-            description: "**Pick one small object:** Think bottle, mug, fruit, plant, TV remote or book."),
-        OnboardingCardDescriptionModel(
-            icon: "rectangle.on.rectangle.slash.fill",
-            description: "**Clear the background:** Use a plain, uncluttered space to keep distractions away."),
-        OnboardingCardDescriptionModel(
-            icon: "viewfinder",
-            description: "**Frame It Right:** Make sure your object fits in the viewfinder before capturing a photo."),
-        OnboardingCardDescriptionModel(
-            icon: "arrow.trianglehead.2.clockwise.rotate.90",
-            description: "**Show different angles:** Snap multiple shots to give AI a complete view."),
-//        OnboardingCardDescriptionModel(
-//            icon: "sun.max.fill",
-//            description: "**Light it up:** Make sure the area is well lit.")
+      OnboardingCardDescriptionModel(
+        icon: "cube.box.fill",
+        description: "**Pick one small object:** Think bottle, mug, fruit, plant, remote, or book."
+      ),
+      OnboardingCardDescriptionModel(
+        icon: "rectangle.on.rectangle.slash.fill",
+        description: "**Clear the background:** Use a plain, simple space."
+      ),
+      OnboardingCardDescriptionModel(
+        icon: "viewfinder",
+        description: "**Frame it:** Ensure your object fits in the viewfinder."
+      ),
+      OnboardingCardDescriptionModel(
+        icon: "arrow.trianglehead.2.clockwise.rotate.90",
+        description: "**Show different angles:** Snap multiple shots for a better AI training."
+      ),
     ]
     
     let card: OnboardingCardModel
