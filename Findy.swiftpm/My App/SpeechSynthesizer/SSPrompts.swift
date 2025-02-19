95d7dd5d549970c14decad43ee7bc67ddc53c3e7
Matvei Novoselov <matveynovoselov@gmail.com>
2025-02-19 21:36:50 +0100

    fixed amount of photos in prompt

--- Findy.swiftpm/My App/SpeechSynthesizer/SSPrompts.swift
+++ Findy.swiftpm/My App/SpeechSynthesizer/SSPrompts.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum SSPrompts {
-    static let captureFirstItem = "Let's capture your first item! Choose an object and take 10 photos from different angles."
+    static let captureFirstItem = "Let's capture your first item! Choose an object and take \(AppMetrics.maxPhotoArrayCapacity) photos from different angles."
     static let trainAI = "Great shot! Now, could you take a few more photos of this item?"
     static let halfway = "We're halfway there—just \(AppMetrics.maxPhotoArrayCapacity/2) more photos to go!"
     static let searching = "Searching for your item. Please move your iPad slowly and show me different views of your room."
