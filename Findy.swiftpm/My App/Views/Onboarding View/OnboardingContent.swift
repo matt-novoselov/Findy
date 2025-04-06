2a2b594255c355453e4b47b42e8d426fb28ecf99
Matvei Novoselov <matveynovoselov@gmail.com>
2025-04-06 15:39:08 +0200

    Changes to the onboarding text

--- Findy.swiftpm/My App/Views/Onboarding View/OnboardingContent.swift
+++ Findy.swiftpm/My App/Views/Onboarding View/OnboardingContent.swift
@@ -6,9 +6,9 @@ struct OnboardingContent {
     let texts: [String] = [
         "This is how most of us see the world…",
         "… and this is how over 295 million people with vision loss experience it every day.",
-        "My grandfather, who taught me to appreciate life's small beauties, gradually lost his sight to aging eyes. Simple tasks — like finding his favorite mug or book - became true daily struggles.",
+        "Simple tasks — like finding a favorite mug or book — can become daily struggles.",
         "But what if technology and AI could help bridge that gap?",
         "...",
-        "For my Grandpa, and for anyone who’s ever felt lost in their own home. You deserve to find your way ♡"
+        "Created for anyone who’s ever felt lost in their own home. You deserve to find your way ♡"
     ]
 }
